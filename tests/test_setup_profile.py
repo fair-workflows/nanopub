c67e45ac5436168bbe291aae791fa54d67e5b0a5
Robin Richardson <raarichardson@gmail.com>
2020-12-04 19:28:03 +0100

    Test setup_profile publishing of intro nanopub

--- tests/test_setup_profile.py
+++ tests/test_setup_profile.py
@@ -39,12 +39,13 @@ def test_provided_keypair_copied_to_nanopub_dir(tmp_path: Path):
     with patch('nanopub.setup_profile.USER_CONFIG_DIR', nanopub_path), \
             patch('nanopub.setup_profile.DEFAULT_PUBLIC_KEY_PATH', new_public_keyfile), \
             patch('nanopub.setup_profile.DEFAULT_PRIVATE_KEY_PATH', new_private_keyfile), \
-            patch('nanopub.profile.PROFILE_PATH', nanopub_path / 'profile.yml'):
+            patch('nanopub.profile.PROFILE_PATH', nanopub_path / 'profile.yml'), \
+            patch('nanopub.setup_profile.NanopubClient.publish') as mocked_client_publish:
         setup_profile.main(
             args=['--keypair', str(custom_public_key_path), str(custom_private_key_path),
                   '--name',  NAME,
                   '--orcid_id', TEST_ORCID_ID,
-                  '--no-publish'],
+                  '--publish'],
             standalone_mode=False)
 
     nanopub_path = mock_homedir / NANOPUB_DIR
@@ -56,6 +57,9 @@ def test_provided_keypair_copied_to_nanopub_dir(tmp_path: Path):
     assert new_public_keyfile.read_text() == MOCK_PUBLIC_KEY
     assert new_private_keyfile.exists()
     assert new_private_keyfile.read_text() == MOCK_PRIVATE_KEY
+
+    # Check that intro nanopub was 'published'
+    assert(mocked_client_publish.called)
 
 
 def test_create_this_is_me_rdf():
