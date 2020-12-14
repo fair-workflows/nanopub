ba9cf2279602376124b45bd35f7ec868e6865acb
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-12-14 15:50:13 +0100

    Remove redundant tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -98,53 +98,6 @@ class TestNanopubClient:
 
     @pytest.mark.flaky(max_runs=10)
     @skip_if_nanopub_server_unavailable
-    def test_find_valid_signed_nanopubs_with_text(self):
-        """
-        Check that Nanopub text search is returning results for a few common search terms
-        for signed and not retracted nanopubs
-        """
-        searches = ['test', 'US']
-
-        for search in searches:
-            results = client.find_valid_signed_nanopubs_with_text(search)
-            assert len(results) > 0
-
-        assert len(client.find_valid_signed_nanopubs_with_text('')) == 0
-
-    @pytest.mark.flaky(max_runs=10)
-    @skip_if_nanopub_server_unavailable
-    def test_find_valid_signed_nanopubs_with_pattern(self):
-        """
-            Check that Nanopub pattern search is returning results
-            for signed and not retracted nanopubs
-        """
-        searches = [
-            ('', '', ''),
-            ('', '', 'http://purl.org/net/p-plan#Plan'),
-            ('', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-                'http://purl.org/net/p-plan#Plan')
-        ]
-
-        for subj, pred, obj in searches:
-            results = client.find_valid_signed_nanopubs_with_pattern(subj=subj, pred=pred, obj=obj)
-            assert len(results) > 0
-            assert 'Error' not in results[0]
-
-    @pytest.mark.flaky(max_runs=10)
-    @skip_if_nanopub_server_unavailable
-    def test_nanopub_find_valid_signed_things(self):
-        """
-        Check that Nanopub 'find_things' search is returning results
-        for signed and not retracted nanopubs
-        """
-        results = client.find_valid_signed_things(type='http://purl.org/net/p-plan#Plan')
-        assert len(results) > 0
-
-        with pytest.raises(Exception):
-            client.find_valid_signed_things()
-
-    @pytest.mark.flaky(max_runs=10)
-    @skip_if_nanopub_server_unavailable
     def test_find_things_pubkey(self):
         results = client.find_things(type='http://purl.org/net/p-plan#Plan', pubkey=PUBKEY)
         assert len(results) > 0
