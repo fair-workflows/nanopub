af57a38144228d45a1237cb48a859a316107bd52
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-12-23 10:45:23 +0100

    Fix error message when RSA keys are missing

--- nanopub/java_wrapper.py
+++ nanopub/java_wrapper.py
@@ -35,7 +35,7 @@ class JavaWrapper:
         rsa_key_messages = ['FileNotFoundException', 'id_rsa']
         stderr = result.stderr.decode('utf8')
         if all(m in stderr for m in rsa_key_messages):
-            raise RuntimeError('Nanopub RSA keys appear to be missing,\n'
+            raise RuntimeError('Nanopub RSA key appears to be missing,\n'
                                + PROFILE_INSTRUCTIONS_MESSAGE
                                + '\nDetailed error message:\n' + stderr)
         elif result.returncode != 0:
