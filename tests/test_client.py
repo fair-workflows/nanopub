5a8fba99c1b05dd73c80033895e7ad7c49203e0e
Robin Richardson <raarichardson@gmail.com>
2021-01-08 14:39:52 +0100

    Add test for non-JSON response being handled correctly

--- tests/test_client.py
+++ tests/test_client.py
@@ -56,6 +56,18 @@ class TestNanopubClient:
 
     @pytest.mark.flaky(max_runs=10)
     @skip_if_nanopub_server_unavailable
+    def test_find_nanopubs_with_text_json_not_returned(self):
+        """
+        Check that text search that triggers a virtuoso error is handled correctly. In such a
+        case HTML is returned by the server rather than JSON.
+        """
+        results = client.find_nanopubs_with_text(
+            'some random string that is not in any of the nanopublications and that virtuoso does not like')
+        assert len(list(results)) == 0
+
+
+    @pytest.mark.flaky(max_runs=10)
+    @skip_if_nanopub_server_unavailable
     def test_find_nanopubs_with_pattern(self):
         """
             Check that Nanopub pattern search is returning results
