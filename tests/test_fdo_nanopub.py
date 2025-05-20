6d1054509bd31ec5d057ac14feeda1af33556a10
Virginia Balseiro <info@virginiabalseiro.com>
2025-05-20 19:45:33 +0200

    Update FDOnanopub tests

--- tests/test_fdo_nanopub.py
+++ tests/test_fdo_nanopub.py
@@ -3,6 +3,7 @@ import rdflib
 from rdflib import RDF, RDFS
 from nanopub.namespaces import HDL, FDOF, NPX
 from nanopub.fdo_nanopub import FDONanopub, to_hdl_uri 
+from nanopub.constants import FDO_DATA_REF_HANDLE_URI, FDO_PROFILE_HANDLE_URI, FDO_SERVICE_HANDLE_URI, FDO_TYPE_HANDLE_URI, FDO_STATUS_HANDLE_URI
 
 FAKE_HANDLE = "21.T11966/test"
 FAKE_URI = HDL[FAKE_HANDLE]
@@ -11,7 +12,6 @@ FAKE_TYPE_JSON = '{"@type": "Dataset"}'
 FAKE_STATUS = "active"
 FAKE_ATTR_VALUE = rdflib.Literal("some value")
 FAKE_ATTR_LABEL = "Test Attribute"
-FDO_PROFILE_HANDLE = HDL['21.T11966/FdoProfile']
 
 
 @pytest.mark.parametrize("fdo_id", [FAKE_HANDLE, HDL[FAKE_HANDLE]])
@@ -30,24 +30,24 @@ def test_add_fdo_profile(fdo_profile):
     fdo = FDONanopub(FAKE_HANDLE, FAKE_LABEL)
     uri = to_hdl_uri(fdo_profile)
     fdo.add_fdo_profile(fdo_profile)
-    assert (fdo.fdo_uri, fdo.FDO_PROFILE_HANDLE, rdflib.Literal(uri)) in fdo.assertion
-    assert (fdo.FDO_PROFILE_HANDLE, RDFS.label, rdflib.Literal("FdoProfile")) in fdo.pubinfo
+    assert (fdo.fdo_uri, FDO_PROFILE_HANDLE_URI, rdflib.Literal(uri)) in fdo.assertion
+    assert (FDO_PROFILE_HANDLE_URI, RDFS.label, rdflib.Literal("FdoProfile")) in fdo.pubinfo
 
 @pytest.mark.parametrize("data_ref", [FAKE_HANDLE, HDL[FAKE_HANDLE]])
 def test_add_fdo_data_ref(data_ref):
     fdo = FDONanopub(FAKE_HANDLE, FAKE_LABEL)
     uri = to_hdl_uri(data_ref)
     fdo.add_fdo_data_ref(data_ref)
-    assert (fdo.fdo_uri, fdo.DATA_REF_HANDLE, uri) in fdo.assertion
-    assert (fdo.DATA_REF_HANDLE, RDFS.label, rdflib.Literal("DataRef")) in fdo.pubinfo
+    assert (fdo.fdo_uri, FDO_DATA_REF_HANDLE_URI, uri) in fdo.assertion
+    assert (FDO_DATA_REF_HANDLE_URI, RDFS.label, rdflib.Literal("DataRef")) in fdo.pubinfo
 
 @pytest.mark.parametrize("service_uri", [FAKE_HANDLE, HDL[FAKE_HANDLE]])
 def test_add_fdo_service(service_uri):
     fdo = FDONanopub(FAKE_HANDLE, FAKE_LABEL)
     uri = to_hdl_uri(service_uri)
     fdo.add_fdo_service(service_uri)
-    assert (fdo.fdo_uri, fdo.FDO_SERVICE_HANDLE, rdflib.Literal(uri)) in fdo.assertion
-    assert (fdo.FDO_SERVICE_HANDLE, RDFS.label, rdflib.Literal("FdoService")) in fdo.pubinfo
+    assert (fdo.fdo_uri, FDO_SERVICE_HANDLE_URI, rdflib.Literal(uri)) in fdo.assertion
+    assert (FDO_SERVICE_HANDLE_URI, RDFS.label, rdflib.Literal("FdoService")) in fdo.pubinfo
 
 @pytest.mark.parametrize("attr_HANDLE", [FAKE_HANDLE, HDL[FAKE_HANDLE]])
 def test_add_attribute_and_label(attr_HANDLE):
@@ -63,10 +63,10 @@ def test_add_fdo_type():
     fdo.add_fdo_type(FAKE_TYPE_JSON)
     # Related to https://github.com/RDFLib/rdflib/issues/1830
     # assert (fdo.fdo_uri, fdo.FDO_TYPE_HANDLE, rdflib.Literal(FAKE_TYPE_JSON, datatype=XSD.string)) in fdo.assertion
-    assert (fdo.FDO_TYPE_HANDLE, RDFS.label, rdflib.Literal("FdoType")) in fdo.pubinfo
+    assert (FDO_TYPE_HANDLE_URI, RDFS.label, rdflib.Literal("FdoType")) in fdo.pubinfo
 
 def test_add_fdo_status():
     fdo = FDONanopub(FAKE_HANDLE, FAKE_LABEL)
     fdo.add_fdo_status(FAKE_STATUS)
-    assert (fdo.fdo_uri, fdo.FDO_STATUS_HANDLE, rdflib.Literal(FAKE_STATUS)) in fdo.assertion
-    assert (fdo.FDO_STATUS_HANDLE, RDFS.label, rdflib.Literal("FdoStatus")) in fdo.pubinfo
+    assert (fdo.fdo_uri, FDO_STATUS_HANDLE_URI, rdflib.Literal(FAKE_STATUS)) in fdo.assertion
+    assert (FDO_STATUS_HANDLE_URI, RDFS.label, rdflib.Literal("FdoStatus")) in fdo.pubinfo
